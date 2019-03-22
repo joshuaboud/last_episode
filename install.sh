8a8b70f11c38da55f4073106c902305ff9171094
joshuaboud <46575851+joshuaboud@users.noreply.github.com>
2019-03-22 13:53:09 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ $(id -u) != 0 ]]; then
+    echo "Script must be run as root."
+    exit 1
+fi
+
+cp lastep.sh /bin/lastep.sh
+
+echo "[Unit]
+Description=Launches lastep.sh with directories to report last accessed episode in
+After=network.target
+
+[Service]
+Type=oneshot" >> /etc/systemd/system/lastep.service
+
+echo "List directories to be checked separated by spaces:"
+read dirs
+
+for dir in $dirs; do
+  echo "ExecStart=/bin/lastep.sh $dir" >> /etc/systemd/system/lastep.service
+done
+
+echo "[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/lastep.service
+
+systemctl enable lastep
